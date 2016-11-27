165851173c3036bae23a2b0c9dc75029f4face3d
Daniel Fehrm <daniel@fehrm.se>
2016-11-27 15:06:34 +0100

    Output the quote to the page

--- js/script.js
+++ js/script.js
@@ -25,6 +25,8 @@ function printQuote(){
     fullQuote += '<span class = "year">' + selectedQuote.year + '</span>';
 
   fullQuote += '</p>';
+
+  document.getElementById('quote-box').innerHTML = fullQuote;
 }
 // event listener to respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
