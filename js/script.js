30c4d165167504206c6dc55f63a556a81e09e3b4
Daniel Fehrm <daniel@fehrm.se>
2016-11-27 14:38:53 +0100

    Add printQuote + rename random variable

--- js/script.js
+++ js/script.js
@@ -8,11 +8,13 @@ var quotes = [
 
 //Selects a random quote from the quotes array and retruns it
 function getRandomQuote(){
-  var random = Math.floor(Math.random() * quotes.length) + 1;
-  return random;
+  var randomQuote = Math.floor(Math.random() * quotes.length);
+  return randomQuote;
 }
 
-
+function printQuote(){
+  var fullQuote = quotes[getRandomQuote()].quote;
+}
 // event listener to respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
