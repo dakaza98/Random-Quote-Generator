98459138d44c4f7828c4d835cd8e9d226ef85651
Daniel Fehrm <daniel@fehrm.se>
2016-11-27 14:48:28 +0100

    Add style for quotes + add citation and source

--- js/script.js
+++ js/script.js
@@ -14,7 +14,11 @@ function getRandomQuote(){
 
 function printQuote(){
   var quoteIndex = getRandomQuote();
-  var fullQuote = quotes[quoteIndex].quote;
+  var fullQuote = '<p class = "quote">' + quotes[quoteIndex].quote + '</p>';
+  fullQuote += '<p class = "source">' + quotes[quoteIndex].source;
+
+  if (quotes[quoteIndex].citation !== null)
+    fullQuote += '<span class = "citation">' + quotes[quoteIndex].citation + '</span>'
 }
 // event listener to respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
