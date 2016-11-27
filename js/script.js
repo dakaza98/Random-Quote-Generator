778bb4a48e3f3ff9c0465f5ddde78ba2d3dc1701
Daniel Fehrm <daniel@fehrm.se>
2016-11-27 18:22:04 +0100

    Add algorithm for showing all quotes before showing one that has already been displayed
    Also adds transition to body

--- js/script.js
+++ js/script.js
@@ -5,16 +5,24 @@ var quotes = [
   {quote: "If I'm not back in five minutes, just wait longer.", source: 'Ace Ventura', citation: 'Ace Ventura: Pet Detective', year: '1994', tags: ['Humor','Movie Quote']},
   {quote: "If we die for them, Harry I'm going to kill you!", source: 'Ron Weasly', citation: 'Harry Potter and the deathly hallows Part 2', year: '2011', tags: ['Humor', 'Movie Quote']}
 ];
+var usedQuotes = [];                      //usedQuotes contains the indexes of the quotes that have not been outputed to the page
 
+function fillUsedQuotes(){
+  for (var i = 0; i < quotes.length; i++)
+    usedQuotes.push(i);                  //Each time usedQuotes is empty it gets refilled to
+}
+
+//Creates a random RGB value and returns rgb(value1, value2, value3)
 function randomRGB(){
   var RGB = 'rgb(';
   for (var i = 0; i < 3; i++){
-    RGB += Math.floor(Math.random() * 256);
+    RGB += Math.floor(Math.random() * 256);       //Creates a random value for each of the colors between and including 0 and 255
+
     if(i !== 2)
-      RGB += ',';
+      RGB += ',';                                 //Adds a comma after the first two values
 
     else
-      RGB += ')';
+      RGB += ')';                                //Ends the paranthesis
   }
   return RGB;
 }
@@ -26,27 +34,35 @@ function changeBackgroundColor(){
 
 //Selects a random quote from the quotes array and returns it
 function getRandomQuote(){
-  var randomQuoteIndex = Math.floor(Math.random() * quotes.length);
-  return quotes[randomQuoteIndex];
+  if(usedQuotes.length === 0)
+    fillUsedQuotes();
+
+  var randomQuoteIndex = usedQuotes.splice(Math.floor(Math.random() * usedQuotes.length), 1);
+  var selectedQuote = quotes[randomQuoteIndex];
+  console.log('index:', randomQuoteIndex[0], selectedQuote.quote)
+
+  return selectedQuote;
 }
 
+//Creates the html code for the quote
 function printQuote(){
   var selectedQuote = getRandomQuote();
 
   var fullQuote = '<p class = "quote">' + selectedQuote.quote + '</p>';
   fullQuote += '<p class = "source">' + selectedQuote.source;
 
-  if (selectedQuote.citation != null)
+  if (selectedQuote.citation != null)                             //If there is no citation declared it wont be outputed
     fullQuote += '<span class = "citation">' + selectedQuote.citation + '</span>';
 
-  if (selectedQuote.year != null)
+  if (selectedQuote.year != null)                                 //If there is no year declared it wont be outputed
     fullQuote += '<span class = "year">' + selectedQuote.year + '</span>';
 
   fullQuote += '</p>';
 
-  changeBackgroundColor();
-  document.getElementById('quote-box').innerHTML = fullQuote;
+  changeBackgroundColor();                                        //Changes the background color
+  document.getElementById('quote-box').innerHTML = fullQuote;     //Adds the quote to the page
 }
+
 // event listener to respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
